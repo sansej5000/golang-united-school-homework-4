b81ab4d107646df2806b98c2ef85239a6f4e9620
sansej5000 <99507931+sansej5000@users.noreply.github.com>
2022-05-17 15:01:22 +0300

    Update string_sum.go
    final

--- string_sum.go
+++ string_sum.go
@@ -1,80 +1,47 @@
 package string_sum
-//packege strconv
 
 import (
 	"errors"
-	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
-//используйте эти ошибки соответствующим образом, обернув их функцией fmt.Errorf
 var (
-	re = regexp.MustCompile(`(?m)([-+]{0,})\s{0,}(\d{1,})\s{0,}([-+]{1,})\s{0,}(\d{1,})`)
-	// Используйте, когда ввод пуст, и ввод считается пустым, если строка содержит только пробелы
+	re = regexp.MustCompile(`(?m)([-+]{0,})([\d\w]{1,})([-+]{1,})([\d\w]{1,})`)
 	errorEmptyInput = errors.New("input is empty")
-	// Используйте, когда выражение имеет количество операндов, не равное двум
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
 )
 
-// Реализуйте функцию, которая вычисляет сумму двух целых чисел, записанных в виде строки.
-// Например, имея входную строку «3+5», он должен возвращать выходную строку «8» и нулевую ошибку.
-// Рассмотрим случаи, когда операнды отрицательные ("-3+5" или "-3-5") и когда входная строка содержит пробелы ("3 + 5").
-//
-//Для случаев, когда входное выражение неверно (содержит символы, не являющиеся числами, +, - или пробел)
-// функция должна возвращать пустую строку и соответствующую ошибку из пакета strconv, завернутую в вашу собственную ошибку
-// с функцией fmt.Errorf
-//
-// Используйте ошибки, определенные выше, как описано, снова завернув в fmt.Errorf
-
 func StringSum(input string) (output string, err error) {
-
-	var one, two, three, four string
-	var two_dig, four_dig int
-
+	var one_str, two_str, three_str, four_str string
+	var sum int
 	whitespace := strings.ReplaceAll(input, " ", "")
-
-	fmt.Println(len(whitespace))
-
 	if len(whitespace) == 0 {
-		fmt.Println("input is empty")
+		return "", fmt.Errorf("%w", errorEmptyInput)
+	} else {
+		for _, match := range re.FindAllStringSubmatch(whitespace, -1) {
+			one_str = match[1]
+			two_str = match[2]
+			three_str = match[3]
+			four_str = match[4]
+		}
+		if one_str != "-" && one_str != "+" && one_str != "" || three_str != "-" && three_str != "+" {
+			return "", fmt.Errorf("%w", errorNotTwoOperands)
+		} else {
+			two_dig, err := strconv.Atoi(one_str + two_str)
+			if err != nil {
+				return "", fmt.Errorf("%w", err)
+			}
+			four_dig, err := strconv.Atoi(three_str + four_str)
+			if err != nil {
+				return "", fmt.Errorf("%w", err)
+			} else {
+				sum = two_dig + four_dig
+				output = strconv.Itoa(sum)
+				return output, nil
+			}
+		}
 	}
-
-	for _, match := range re.FindAllStringSubmatch(input, -1) {
-		one = match[1]
-		two = match[2]
-		three = match[3]
-		four = match[4]
-	}
-
-
-	fmt.Println(one)
-	fmt.Println(two)
-	fmt.Println(three)
-	fmt.Println(four)
-	
-	if one_str != "-" && one_str != "+" && one_str != "" || three_str != "-" && three_str != "+" {
-		fmt.Println("expecting two operands, but received more or less")
-	}
-
-	two_dig, _ = strconv.Atoi(one_str + two_str)
-	//if err == nil {
-	//	fmt.Println("\nAfter:")
-	//	fmt.Printf("Type: %T ", two_dig)
-	//	fmt.Printf("\nValue: %v", two_dig)
-	//}
-
-	four_dig, _ = strconv.Atoi(three_str + four_str)
-	//fmt.Println(two_dig)
-	//fmt.Println(four_dig)
-
-	sum := two_dig + four_dig
-	fmt.Println("Сумма:", sum)
-	
-	return "", nil
 }
 
-func main() {
-	itog := StringSum("-3+5")
-	fmt.Println(itog)
-}
