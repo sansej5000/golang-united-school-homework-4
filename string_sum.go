ecce5c84e6cefb263a601ab4eea43644f719fdfe
sansej5000 <99507931+sansej5000@users.noreply.github.com>
2022-05-18 08:45:18 +0300

    Update string_sum.go

--- string_sum.go
+++ string_sum.go
@@ -16,33 +16,28 @@ var (
 
 func StringSum(input string) (output string, err error) {
 	var one_str, two_str, three_str, four_str string
-	var sum int
 	whitespace := strings.ReplaceAll(input, " ", "")
 	if len(whitespace) == 0 {
 		return "", fmt.Errorf("%w", errorEmptyInput)
-	} else {
-		for _, match := range re.FindAllStringSubmatch(whitespace, -1) {
-			one_str = match[1]
-			two_str = match[2]
-			three_str = match[3]
-			four_str = match[4]
-		}
-		if one_str != "-" && one_str != "+" && one_str != "" || three_str != "-" && three_str != "+" {
-			return "", fmt.Errorf("%w", errorNotTwoOperands)
-		} else {
-			two_dig, err := strconv.Atoi(one_str + two_str)
-			if err != nil {
-				return "", fmt.Errorf("%w", err)
-			}
-			four_dig, err := strconv.Atoi(three_str + four_str)
-			if err != nil {
-				return "", fmt.Errorf("%w", err)
-			} else {
-				sum = two_dig + four_dig
-				output = strconv.Itoa(sum)
-				return output, nil
-			}
-		}
 	}
+	for _, match := range re.FindAllStringSubmatch(whitespace, -1) {
+		one_str = match[1]
+		two_str = match[2]
+		three_str = match[3]
+		four_str = match[4]
+	}
+	if len(one_str) > 1 || len(three_str) > 1 {
+		return "", fmt.Errorf("%w", errorNotTwoOperands)
+	}
+	two_dig, err := strconv.Atoi(one_str + two_str)
+	if err != nil {
+		return "", fmt.Errorf("%w", err)
+	}
+	four_dig, err := strconv.Atoi(three_str + four_str)
+	if err != nil {
+		return "", fmt.Errorf("%w", err)
+	}
+	output = strconv.Itoa(two_dig + four_dig)
+	return output, nil
 }
 
