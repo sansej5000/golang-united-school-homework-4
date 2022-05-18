b6775c7465a45de0df9cfeb5c552b5f91bdf95f4
sansej5000 <99507931+sansej5000@users.noreply.github.com>
2022-05-18 11:49:55 +0300

    Update test_string_sum.go

--- test_string_sum.go
+++ test_string_sum.go
@@ -3,7 +3,7 @@ import "tasting"
 func StringEmptyTest(t *testing. T){
   str:=" "
   _,err:=StringSum(str)
-  if err!=nil{
-    t.Errorf("Str is empty: %s", err)
+  if err==nil{
+    t.Errorf("result is wrong, the result should be 'input is empty'")
   }
 }
