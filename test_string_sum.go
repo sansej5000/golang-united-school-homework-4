ea96153ff54a512578ce976bf232092a5f336cc6
sansej5000 <99507931+sansej5000@users.noreply.github.com>
2022-05-18 11:54:09 +0300

    Update test_string_sum.go

--- test_string_sum.go
+++ test_string_sum.go
@@ -1,9 +1,17 @@
-import "tasting"
+import "testing"
 
 func StringEmptyTest(t *testing. T){
   str:=" "
   _,err:=StringSum(str)
   if err==nil{
     t.Errorf("result is wrong, the result should be 'input is empty'")
+  }
+}
+
+func LetterInFirstOperandTest(t *testing. T){
+  str:="2s+5"
+  _,err:=StringSum(str)
+  if err==nil{
+    t.Errorf("result is wrong, the result should be 'strconv.Atoi: parsing '2s': invalid syntax'")
   }
 }
