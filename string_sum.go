d7dcbc89568d4338f02aae0054f79af727f67438
sansej5000 <99507931+sansej5000@users.noreply.github.com>
2022-05-18 11:17:05 +0300

    Update string_sum.go

--- string_sum.go
+++ string_sum.go
@@ -9,35 +9,36 @@ import (
 )
 
 var (
-	re = regexp.MustCompile(`(?m)([-+]{0,})([\d\w]{1,})([-+]{1,})([\d\w]{1,})`)
-	errorEmptyInput = errors.New("input is empty")
+	re                  = regexp.MustCompile(`(?m)([-+]\d+)?([-+]\d+)?([-+]\d+)?`)
+	errorEmptyInput     = errors.New("input is empty")
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
 )
 
 func StringSum(input string) (output string, err error) {
-	var one_str, two_str, three_str, four_str string
+	var one_str, two_str string
 	whitespace := strings.ReplaceAll(input, " ", "")
 	if len(whitespace) == 0 {
 		return "", fmt.Errorf("%w", errorEmptyInput)
 	}
 	for _, match := range re.FindAllStringSubmatch(whitespace, -1) {
 		one_str = match[1]
+		fmt.Println(one_str)
 		two_str = match[2]
-		three_str = match[3]
-		four_str = match[4]
+		fmt.Println(two_str)
+
+		if match[2] == "" || match[3] != "" {
+			return "", fmt.Errorf("%w", errorNotTwoOperands)
+		}
 	}
-	if len(one_str) > 1 || len(three_str) > 1 {
-		return "", fmt.Errorf("%w", errorNotTwoOperands)
-	}
-	two_dig, err := strconv.Atoi(one_str + two_str)
+	one_dig, err := strconv.Atoi(one_str)
 	if err != nil {
 		return "", fmt.Errorf("%w", err)
 	}
-	four_dig, err := strconv.Atoi(three_str + four_str)
+	two_dig, err := strconv.Atoi(two_str)
 	if err != nil {
 		return "", fmt.Errorf("%w", err)
 	}
-	output = strconv.Itoa(two_dig + four_dig)
+	output = strconv.Itoa(one_dig + two_dig)
 	return output, nil
 }
 
