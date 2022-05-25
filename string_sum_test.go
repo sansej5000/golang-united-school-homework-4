8e855bbd1ee485e2a9d156e8d2bbc831473065d2
sansej5000 <nrcminsk@gmail.com>
2022-05-25 22:38:20 +0300

    update

---
+++ string_sum_test.go
@@ -0,0 +1,94 @@
+package string_sum
+
+import (
+	"testing"
+)
+
+func TestEmptyInput(t *testing.T) {
+	str := " "
+	_, err := StringSum(str)
+	if err == nil {
+		t.Errorf("result is wrong, the result should be 'input is empty'")
+	}
+}
+
+func TestLetterInFirstOperand(t *testing.T) {
+	str := "2s+5"
+	_, err := StringSum(str)
+	if err == nil {
+		t.Errorf("result is wrong, the result should be 'strconv.Atoi: parsing '2s': invalid syntax'")
+	}
+}
+
+func TestLetterInSecondOperand(t *testing.T) {
+	str := "2+5s"
+	_, err := StringSum(str)
+	if err == nil {
+		t.Errorf("result is wrong, the result should be 'strconv.Atoi: parsing '5s': invalid syntax'")
+	}
+}
+
+func TestFirstOperandNegative(t *testing.T) {
+	str := "-2+5"
+	res, _ := StringSum(str)
+	if res != "3" {
+		t.Errorf("result is wrong, the result should be '3'")
+	}
+}
+
+func TestSecondOperandNegative(t *testing.T) {
+	str := "2-5"
+	res, _ := StringSum(str)
+	if res != "-3" {
+		t.Errorf("result is wrong, the result should be '-3'")
+	}
+}
+
+func TestBothOperandNegative(t *testing.T) {
+	str := "-2-5"
+	res, _ := StringSum(str)
+	if res != "-7" {
+		t.Errorf("result is wrong, the result should be '-7'")
+	}
+}
+
+func TestBothOperandPositive(t *testing.T) {
+	str := "2+5"
+	res, _ := StringSum(str)
+	if res != "7" {
+		t.Errorf("result is wrong, the result should be '7'")
+	}
+}
+
+func TestWithWhispace(t *testing.T) {
+	str := " 2 + 5 "
+	res, _ := StringSum(str)
+	if res != "7" {
+		t.Errorf("result is wrong, the result should be '7'")
+	}
+}
+
+func TestThreeOperands(t *testing.T) {
+	str := "2+5-3 "
+	_, err := StringSum(str)
+	if err == nil {
+		t.Errorf("result is wrong, the result should be 'expecting two operands, but received more or less'")
+	}
+}
+
+func TestOneOperands(t *testing.T) {
+	str := "-3 "
+	_, err := StringSum(str)
+	if err == nil {
+		t.Errorf("result is wrong, the result should be 'expecting two operands, but received more or less'")
+	}
+}
+
+// func StringSumTest(t *testing.T) {
+// 	str := "2+3"
+// 	res, _ := StringSum(str)
+// 	rst, _ := fmt.Printf("%T", res)
+// 	if rst != "string6" {
+// 		t.Errorf("result is wrong, the result should be 'string6'")
+// 	}
+// }
